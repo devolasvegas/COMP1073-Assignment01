7b1242a7977f50655bcaec9f3b90b3c27b37c1e1
DEVON DAVIAU <devon.daviau@yahoo.ca>
2016-02-10 13:08:24 -0500

    Initial Commit - Basic folder structure and files.

---
+++ Scripts/app.js
@@ -0,0 +1,35 @@
+/**
+ * Created by devon on 2016-02-04.
+ */
+
+"use strict";
+
+// Set up your IIFE (Immediately Invoked Function Expression) or ASEF (Anonymous Self-Executing Function)
+(function(){
+    "use strict";
+    console.log("App Started . . .");
+
+    /*
+    // named function way
+    function replaceFirstParagraph(){
+        console.log("Inside replaceFirstParagraph function.");
+        var firstParagraph;
+        firstParagraph = document.getElementById("firstParagraph");
+        firstParagraph.innerHTML = "My New Paragraph Data";
+    }
+     */
+
+
+
+    //declare an anon. function with named alias
+    var replaceFirstParagraph = function(){
+        console.log("Inside replaceFirstParagraph anonymous function.");
+        var firstParagraph;
+        firstParagraph = document.getElementById("firstParagraph");
+        firstParagraph.innerHTML = "Newer content.";
+    }
+
+    // Call replaceFirstParagraph function defined above.
+    replaceFirstParagraph();
+
+})();
