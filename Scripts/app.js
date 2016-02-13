4ccf104064338b9189970efdc65cd488b1866707
DEVON DAVIAU <devon.daviau@yahoo.ca>
2016-02-12 21:37:53 -0500

    Edit index.html and app.js to inject project text.

--- Scripts/app.js
+++ Scripts/app.js
@@ -4,7 +4,7 @@
     "use strict";
     console.log("App Started . . .");
 
-    // Function for inserting bio information to the About Me page.
+    // Function for inserting bio information.
     function insertBio(){
         console.log("Inside insertBio function.");
         var bioContent;
@@ -12,8 +12,37 @@
         bioContent.innerHTML = "<p>I was born in beautiful Barrie, Ontario, and left at a young age to explore the U.S. of A. In my fifteen years there, I lived in Oregon,  North Carolina, and the U.P. of Michigan. Since 2000, I have been back in Ontario.</p><p>I have held many jobs in my life, starting with the distinguished trade of horse-stall cleaner when I was 15. There have been construction jobs, food-service jobs, retail, and administrative jobs. I have finally found what I consider to be a career path that will truly interest and challenge me: WEB DEVELOPMENT.</p><p>I really enjoy that development for the world-wide web can be both technical and creative at the same time. I truly enjoy solving problems, and that is what development is all about. There is also team work and independent work. Web development is a flexible career path that requires a flexible participant. For someone who is curious and enjoys learning like I do, this is ideal.</p>";
     }
 
+    function insertProjectOne(){
+        console.log("Inside insertProjectOne function.");
+        var projectOneContent;
+        projectOneContent = document.getElementById("project-1");
+        projectOneContent.innerHTML = "Retro Brylcreem Website";
+    }
+
+    function insertProjectTwo(){
+        console.log("Inside insertProjectTwo function.");
+        var projectTwoContent;
+        projectTwoContent = document.getElementById("project-2");
+        projectTwoContent.innerHTML = "Personas and Use-Cases";
+    }
+
+    function insertProjectThree(){
+        console.log("Inside insertProjectThree function.");
+        var projectThreeContent;
+        projectThreeContent = document.getElementById("project-3");
+        projectThreeContent.innerHTML = "Clash of Cthulhu";
+    }
 
     // Call replaceFirstParagraph function defined above.
     insertBio();
+
+    // Call insertProjectOne function defined above.
+    insertProjectOne();
+
+    // Call insertProjectTwo function defined above.
+    insertProjectTwo();
+
+    // Call insertProjectThree function defined above.
+    insertProjectThree();
 
 })();
